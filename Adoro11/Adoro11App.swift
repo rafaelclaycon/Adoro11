9539d991f96d5bb35e73d30a6552d44fd498cebf
Rafael Schmitt <rafael.claycon@gmail.com>
2023-12-16 13:45:36 -0300

    Initial Commit

---
+++ Adoro11/Adoro11App.swift
@@ -0,0 +1,17 @@
+//
+//  Adoro11App.swift
+//  Adoro11
+//
+//  Created by Rafael Schmitt on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Adoro11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
